83a572d510556781dbeeb3617c7fcd35190faa6d
aaridaa <ahridah9@gmail.com>
2019-09-17 07:45:48 -0500

    Edited ticket activity

--- ToolBoxAhmed/app/src/main/java/com/example/toolboxahmed/TicketActivity.java
+++ ToolBoxAhmed/app/src/main/java/com/example/toolboxahmed/TicketActivity.java
@@ -8,18 +8,11 @@ import android.widget.TextView;
 
 public class TicketActivity extends AppCompatActivity {
 
-    public static final String REG_PREM = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket);
 
-        Intent intent = getIntent();
-        String y = intent.getStringExtra(REG_PREM);
-
-        String strToDisplay = "Your experience: "+ y;
-        TextView str = (TextView) findViewById(R.id.info);
-        str.setText(strToDisplay);
     }
 }
